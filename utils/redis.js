9e5e44059d7c8527cbfdf747d8cef91a9aec9fa5
Francis Waihiga <francisgwaihiga@gmail.com>
2024-12-09 13:58:15 +0300

    Output error correction

--- utils/redis.js
+++ utils/redis.js
@@ -3,51 +3,30 @@ import { createClient } from 'redis';
 class RedisClient {
     constructor() {
         this.client = createClient();
-        this.ready = false; // Initial connection state is false
-
-        this.client.on('connect', () => {
-            console.log('Redis client connected successfully');
-            this.ready = true; // Set to true when the connection is successful
-        });
-
         this.client.on('error', (err) => {
             console.error('Redis client error:', err);
         });
-
         this.client.connect().catch((err) => {
             console.error('Redis connection error:', err);
         });
     }
 
     isAlive() {
-        return this.client.isOpen && this.ready; // Check both isOpen and ready
+        return this.client.isReady;
     }
 
     async get(key) {
-        try {
-            return await this.client.get(key);
-        } catch (err) {
-            console.error(`Redis GET error: ${err.message}`);
-            return null;
-        }
+        return await this.client.get(key);
     }
 
     async set(key, value, duration) {
-        try {
-            await this.client.set(key, value, {
-                EX: duration,
-            });
-        } catch (err) {
-            console.error(`Redis SET error: ${err.message}`);
-        }
+        await this.client.set(key, value, {
+            EX: duration,
+        });
     }
 
     async del(key) {
-        try {
-            await this.client.del(key);
-        } catch (err) {
-            console.error(`Redis DEL error: ${err.message}`);
-        }
+        await this.client.del(key);
     }
 }
 
