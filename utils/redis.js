de73b9bbadac84106063d552750443559e8ba94e
Ademitidayo <adenijibimmy08@gmail.com>
2024-12-10 02:50:21 +0100

    task 1 and 3

--- utils/redis.js
+++ utils/redis.js
@@ -1,33 +1,53 @@
-import { createClient } from 'redis';
+import redis from 'redis';
 
 class RedisClient {
-    constructor() {
-        this.client = createClient();
-        this.client.on('error', (err) => {
-            console.error('Redis client error:', err);
-        });
-        this.client.connect().catch((err) => {
-            console.error('Redis connection error:', err);
-        });
-    }
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.error(error);
+    });
+  }
 
-    isAlive() {
-        return this.client.isReady;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        return await this.client.get(key);
-    }
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, reply) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
 
-    async set(key, value, duration) {
-        await this.client.set(key, value, {
-            EX: duration,
-        });
-    }
+  async set(key, value, duration) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (error, reply) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(reply);
+        }
+      });
+    });
+  }
 
-    async del(key) {
-        await this.client.del(key);
-    }
+  async del(key) {
+    // eslint-disable-next-line no-unused-vars
+    return new Promise((resolve, _reject) => {
+      this.client.del(key, (error) => {
+        if (error) {
+          resolve(false);
+        } else {
+          resolve(true);
+        }
+      });
+    });
+  }
 }
 
 const redisClient = new RedisClient();
