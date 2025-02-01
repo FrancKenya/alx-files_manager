3a68cf98e79562f6970cb0f92106a3427a5db556
Francis Waihiga <francisgwaihiga@gmail.com>
2025-02-01 04:49:00 +0300

    reverted to the previous changes

--- utils/redis.js
+++ utils/redis.js
@@ -1,76 +1,33 @@
-import redis from 'redis';
-<<<<<<< HEAD
-import { promisify } from 'util';
-=======
->>>>>>> refs/remotes/origin/main
+import { createClient } from 'redis';
 
 class RedisClient {
-  constructor() {
-    this.client = redis.createClient();
-<<<<<<< HEAD
-    this.client.on('error', (err) => console.error('Redis Client Error:', err));
-    this.getAsync = promisify(this.client.get).bind(this.client);
-    this.setAsync = promisify(this.client.set).bind(this.client);
-    this.delAsync = promisify(this.client.del).bind(this.client);
-=======
-    this.client.on('error', (error) => {
-      console.error(error);
-    });
->>>>>>> refs/remotes/origin/main
-  }
+    constructor() {
+        this.client = createClient();
+        this.client.on('error', (err) => {
+            console.error('Redis client error:', err);
+        });
+        this.client.connect().catch((err) => {
+            console.error('Redis connection error:', err);
+        });
+    }
 
-  isAlive() {
-    return this.client.connected;
-  }
+    isAlive() {
+        return this.client.isReady;
+    }
 
-  async get(key) {
-<<<<<<< HEAD
-    return this.getAsync(key);
-  }
+    async get(key) {
+        return await this.client.get(key);
+    }
 
-  async set(key, value, duration) {
-    return this.setAsync(key, value, 'EX', duration);
-  }
+    async set(key, value, duration) {
+        await this.client.set(key, value, {
+            EX: duration,
+        });
+    }
 
-  async del(key) {
-    return this.delAsync(key);
-=======
-    return new Promise((resolve, reject) => {
-      this.client.get(key, (error, reply) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(reply);
-        }
-      });
-    });
-  }
-
-  async set(key, value, duration) {
-    return new Promise((resolve, reject) => {
-      this.client.setex(key, duration, value, (error, reply) => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(reply);
-        }
-      });
-    });
-  }
-
-  async del(key) {
-    // eslint-disable-next-line no-unused-vars
-    return new Promise((resolve, _reject) => {
-      this.client.del(key, (error) => {
-        if (error) {
-          resolve(false);
-        } else {
-          resolve(true);
-        }
-      });
-    });
->>>>>>> refs/remotes/origin/main
-  }
+    async del(key) {
+        await this.client.del(key);
+    }
 }
 
 const redisClient = new RedisClient();
